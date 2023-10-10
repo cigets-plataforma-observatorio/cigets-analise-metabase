c9db47a1cb25980744a6bf1502b87cf84bf28526
Wemerson Marques <wem.dsm@gmail.com>
2023-10-09 22:26:13 -0300

    Corrigindo URL

--- metabase-self-register/api.py
+++ metabase-self-register/api.py
@@ -8,7 +8,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 
 # Variaveis
-metabase_url="http://200.137.215.27:3000/"
+metabase_url="http://200.137.215.27:3000"
 usuario_admin="wdsmarques@gmail.com"
 senha_admin="wandersondsm123"
 
